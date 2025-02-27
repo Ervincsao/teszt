de6c8166c880b5d79831ad97489ae83011462f0e
Lilla624 <vargalilla@ktch.hu>
2025-02-27 14:32:02 +0100

    csao

---
+++ WebshopApi/WebshopApi/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebshopApi.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebshopApiContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebshopApiContext") ?? throw new InvalidOperationException("Connection string 'WebshopApiContext' not found.")));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(b => b.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
